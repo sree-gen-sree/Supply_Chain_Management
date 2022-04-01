40283040b866db8b3d3feb835cffc03c99bfc3f0
SREELAKSHMI A M <amsreelakshmi20@gmail.com>
2022-04-01 12:20:59 +0530

    introduced certificateHash

--- smartContract.sol
+++ smartContract.sol
@@ -1,4 +1,15 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract Supplychain  {
+    enum gender {male, female}
+
+
+mapping(uint => string) public certificateHash;
+   
+    function setCertificate(uint _id, string memory _certificateHash) public {
+        certificateHash[_id] = _certificateHash;
+    }
+     function getCertificate(uint _id) public view returns (string memory _certificateHash ) {
+       _certificateHash = certificateHash[_id] ;
+    }
 }
